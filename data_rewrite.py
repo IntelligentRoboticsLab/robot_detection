a2cf4af8b043aac9e27ccd813adc78b6e8a5132b
MachaMeijer <73819496+MachaMeijer@users.noreply.github.com>
2023-06-07 14:54:08 +0200

    Update data_rewrite.py

--- data_rewrite.py
+++ data_rewrite.py
@@ -7,20 +7,21 @@ with open("coco_ball_nao/bbox.json") as f:
 image_width = data["images"][0]["width"]
 image_height = data["images"][0]["height"]
 
+if not os.path.exists("coco_ball_nao/txtannotations"):
+    os.mkdir("coco_ball_nao/txtannotations")
+
 images = {}
 for image in data["images"]:
     images[image["id"]] = image["file_name"]
-
-if not os.path.exists("coco_ball_nao/txtannotations"):
-    os.mkdir("coco_ball_nao/txtannotations")
+    image_name = image["file_name"].split(".")[0]
+    open(f"coco_ball_nao/txtannotations/{image_name}.txt", "w")
 
 for annotation in data["annotations"]:
     image_id = annotation["image_id"]
     image_name = images[image_id].split(".")[0]
-    
     with open(f"coco_ball_nao/txtannotations/{image_name}.txt", "w") as f:
         bbox1 = annotation['bbox'][0] / image_width
         bbox2 = annotation['bbox'][1] / image_height
         bbox3 = annotation['bbox'][2] / image_width
         bbox4 = annotation['bbox'][3] / image_height
-        f.write(f"{annotation['category_id']} {bbox1} {bbox2} {bbox3} {bbox4}")
+        f.write(f"{annotation['category_id']} {bbox1} {bbox2} {bbox3} {bbox4} \n")
