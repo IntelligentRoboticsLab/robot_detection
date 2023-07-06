4cfb59bc1685ee198ab93f87f89c28244e330584
jurgendeheus <jurgen.de.heus@student.uva.nl>
2023-07-06 18:24:27 +0200

    Update data_rewrite.py

--- data_rewrite.py
+++ data_rewrite.py
@@ -1,37 +1,93 @@
+"""
+Module that splits COCO Nao/Ball dataset by filtering out
+unused images and creates a 80/20 train/val split
+
+TODO: refractor code to not use naive count method for splitting data
+TODO: generate json file for annotations instead of txt file to reduce clutter
+ """
 import json
 import os
 import shutil
 
-with open("data/coco_ball_nao/bbox.json") as f:
+# function to create subfolders for both train and validation splits
+def create_subfolders(parent_folder, subfolders):
+    if not os.path.exists(parent_folder):
+        os.mkdir(parent_folder)
+
+    for subfolder in subfolders:
+        path = os.path.join(parent_folder, subfolder)
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+
+# get COCO format annotations
+with open("data/coco_nao/bbox.json") as f:
     data = json.load(f)
 
-image_width = 1 #data["images"][0]["width"]
-image_height = 1 #data["images"][0]["height"]
+# image width and height for each image
+img_width = data["images"][0]["width"]
+img_height = data["images"][0]["height"]
 
+# dict with file names for each image
 images = {}
 for image in data["images"]:
     images[image["id"]] = image["file_name"]
 
-if not os.path.exists("data/coco_ball_nao/labels/"):
-    os.mkdir("data/coco_ball_nao/labels/")
+# Create train folders
+create_subfolders("data/coco_nao/train", ["labels", "images"])
 
-if not os.path.exists("data/coco_ball_nao/filtered_images"):
-    os.mkdir("data/coco_ball_nao/filtered_images")
+# Create validation folders
+create_subfolders("data/coco_nao/val", ["labels", "images"])
 
-filtered_images_dir = "data/coco_ball_nao/filtered_images/"
+# get total amount of images that are part of desired category
+# accompanied with all the annotations that will be used
+TOTAL = 0
+category_annotations = []
 
 for annotation in data["annotations"]:
+    if annotation['category_id'] == 1:
+        TOTAL += 1
+        category_annotations.append(annotation)
+
+# use 80/20 split
+TRAIN_SPLIT = TOTAL - (int(0.2 * TOTAL))
+COUNT = 0
+
+# loop through annotations that will be used
+for annotation in category_annotations:
     image_id = annotation["image_id"]
     image_name = images[image_id].split(".")[0]
-    image_filename = image_name + ".png"  # Assuming the images have a .jpg extension
+    image_filename = image_name + ".png"
 
-    with open(f"data/coco_ball_nao/labels/{image_name}.txt", "w") as f:
-        bbox1 = annotation['bbox'][0] / image_width
-        bbox2 = annotation['bbox'][1] / image_height
-        bbox3 = annotation['bbox'][2] / image_width
-        bbox4 = annotation['bbox'][3] / image_height
-        f.write(f"{annotation['category_id']} {bbox1} {bbox2} {bbox3} {bbox4}")
+    # check if train split is already filled based on count
+    SPLIT_DIR = "train" if COUNT < TRAIN_SPLIT else "val"
 
-    # image_filepath = os.path.join("data/coco_ball_nao/images", image_filename)  # Provide the path to the separate folder
-    # if os.path.exists(image_filepath):
-    #     shutil.copy(image_filepath, filtered_images_dir)
+    # open a txt file with the annotation for each used image
+    with open(f"data/coco_nao/{SPLIT_DIR}/labels/{image_name}.txt", "w") as f:
+
+        # convert the COCO format annotations into YOLO format
+        # source:
+        # https://haobin-tan.netlify.app/ai/computer-vision/object-detection/coco-json-to-yolo-txt/
+        x_tl, y_tl, w, h = annotation['bbox']
+
+        dw = 1.0 / img_width
+        dh = 1.0 / img_height
+
+        x_center = x_tl + w / 2.0
+        y_center = y_tl + h / 2.0
+
+        x = x_center * dw
+        y = y_center * dh
+        w = w * dw
+        h = h * dh
+
+        # write coordinates to txt file
+        f.write(f"{annotation['category_id']} {x} {y} {w} {h}")
+
+        # copy images from original folder to respective train/val subfolder
+        image_filepath = os.path.join("data/coco_nao/images", image_filename)
+        if os.path.exists(image_filepath):
+            shutil.copy(image_filepath, f"data/coco_nao/{SPLIT_DIR}/images")
+
+        COUNT += 1
+
